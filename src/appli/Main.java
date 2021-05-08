e3d7abc603a6d96d6fb33adef72c0828518648df
Xaela21 <72209227+Xaela21@users.noreply.github.com>
2021-05-08 19:40:02 +0200

    Add files via upload

--- src/appli/Main.java
+++ src/appli/Main.java
@@ -10,9 +10,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("UI.fxml"));
         primaryStage.setTitle("TEAMS Attendees List Converter");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 306, 618));
         primaryStage.show();
     }
 
